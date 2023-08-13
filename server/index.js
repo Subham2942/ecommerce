d7e7113e4ac4dcfce80123a8ab4cd67869403eff
Subham2942 <subham0464@gmail.com>
2023-08-13 19:55:50 +0530

    Stripe integration

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 dotenv.config();
+
+app.use(cors());
 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe")
 
 const url = process.env.MONGODB_URI;
 
@@ -38,3 +42,4 @@ app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
